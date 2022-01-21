c1954800756b904ee27c892b5af1b08d59c79eaa
Patrick Groot <info@patrickgroot.com>
2022-01-21 15:04:11 +0100

    new commit

--- src/components/Task.js
+++ src/components/Task.js
@@ -47,7 +47,7 @@ export const Task = ({
         flex="1 1 auto"
         color={state === 'TASK_ARCHIVED' ? 'gray.400' : 'gray.700'}
         textDecoration={state === 'TASK_ARCHIVED' ? 'line-through' : 'none'}
-        fontSize="md"
+        fontSize="xl"
         isTruncated
         value={title}
         onChange={(e) => onEditTitle(e.target.value, id)}
